6503738988714374e0655f74291aadf203403b9f
Randall Leeds <randall@bleeds.info>
2014-07-29 22:21:49 -0700

    Add Python3 classifier

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
